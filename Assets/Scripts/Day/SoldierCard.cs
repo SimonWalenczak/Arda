001cc23ce7003169afba1546b0878544ef41a6f6
Frostork <92367148+SimonWalenczak@users.noreply.github.com>
2023-01-29 02:01:42 +0100

    ajout grade + blessure sur soldat aléatoire

--- Assets/Scripts/Day/SoldierCard.cs
+++ Assets/Scripts/Day/SoldierCard.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class SoldierCard : MonoBehaviour
 {
@@ -12,6 +14,12 @@ public class SoldierCard : MonoBehaviour
     public TextMeshProUGUI FirstNameText;
     public TextMeshProUGUI AgeText;
     public TextMeshProUGUI SituationText;
+    public TextMeshProUGUI MilitaryRankText;
+    public Image InjurySprite;
+    public int InjuryType;
+
+    public List<float> InjuryTime;
+    private float _resetInjuryTime;
     
 
     private void Update()
@@ -23,6 +31,34 @@ public class SoldierCard : MonoBehaviour
         else
         {
             transform.DOScale(1, 0.2f);
+        }
+
+        switch (InjuryType)
+        {
+            case 1:
+                InjurySprite.color = Color.green;
+                break;
+            
+            case 2:
+                InjurySprite.color = Color.yellow;
+                break;
+            
+            case 3:
+                InjurySprite.color = Color.red;
+                break;
+            
+            case 4:
+                Destroy(gameObject);
+                break;
+        }
+
+        if (InjuryType <= 3)
+        {
+            InjuryTime[InjuryType - 1] -= Time.deltaTime;
+            if (InjuryTime[InjuryType - 1] <= 0)
+            {
+                InjuryType++;
+            }
         }
     }
 }
--- Assets/Scripts/GenerateSoldier.cs
+++ Assets/Scripts/GenerateSoldier.cs
@@ -8,16 +8,19 @@ public class GenerateSoldier : MonoBehaviour
     public List<String> LastName;
     public List<String> FirstName;
     public List<String> Situation;
-    
-    enum States
-    {
-        green,
-        orange,
-        red
-    }
-    public Enum state;
+
+    public List<String> MilitaryRank;
+
     [SerializeField] private float _appearingChance;
     
+    [Header("\nMilitaryClass\n")]
+    [SerializeField] private float _captainClassChance = 10;
+    [SerializeField] private float _majorClassChance = 35;
+    
+    [Header("\nInjuryState\n")]
+    [SerializeField] private float _hardInjuryChance = 10;
+    [SerializeField] private float _mediumInjuryChance = 35;
+
     private Camp _camp;
     private void Start()
     {
@@ -27,19 +30,50 @@ public class GenerateSoldier : MonoBehaviour
     public void GeneratorSoldier()
     {
         int _appearingChanceAppear = Random.Range(0, 101);
-        if (_camp.SoldierInPlace < 8 && _appearingChanceAppear <= _appearingChance)
+        if (_camp.SoldierInPlace < 4 && _appearingChanceAppear <= _appearingChance)
         {
             _camp.SoldierInPlace++;
             string firstName = FirstName[Random.Range(0, FirstName.Count)];
             string lastName = LastName[Random.Range(0, LastName.Count)];
-            int age = Random.Range(18,36);
             string situation = Situation[Random.Range(0, Situation.Count)];
+            int age = 0;
             
-            //Visual
-            _camp._soldierCard[_camp.SoldierInPlace-1].FirstNameText.SetText(firstName);
-            _camp._soldierCard[_camp.SoldierInPlace-1].LastNameText.SetText(lastName);
-            _camp._soldierCard[_camp.SoldierInPlace-1].AgeText.SetText(age.ToString());
-            _camp._soldierCard[_camp.SoldierInPlace-1].SituationText.SetText(situation);
+            int _militaryRankChance = Random.Range(0, 101);
+            if (_militaryRankChance <= _captainClassChance)
+            {
+                _camp._soldierCard[_camp.SoldierInPlace - 1].MilitaryRankText.SetText("Rank : " + MilitaryRank[2]);
+                age = Random.Range(30, 36);
+            }
+            else if (_militaryRankChance <= _majorClassChance)
+            {
+                _camp._soldierCard[_camp.SoldierInPlace - 1].MilitaryRankText.SetText("Rank : " + MilitaryRank[1]);
+                age = Random.Range(25, 36);
+            }
+            else
+            {
+                _camp._soldierCard[_camp.SoldierInPlace - 1].MilitaryRankText.SetText("Rank : " + MilitaryRank[0]);
+                age = Random.Range(18, 36);
+            }
+
+
+            int _injuryChance = Random.Range(0, 101);
+            if (_injuryChance <= _hardInjuryChance)
+            {
+                _camp._soldierCard[_camp.SoldierInPlace - 1].InjuryType = 3;
+            }
+            else if (_injuryChance <= _mediumInjuryChance)
+            {
+                _camp._soldierCard[_camp.SoldierInPlace - 1].InjuryType = 2;
+            }
+            else
+            {
+                _camp._soldierCard[_camp.SoldierInPlace - 1].InjuryType = 1;
+            }
+            
+            _camp._soldierCard[_camp.SoldierInPlace - 1].FirstNameText.SetText(firstName);
+            _camp._soldierCard[_camp.SoldierInPlace - 1].LastNameText.SetText(lastName);
+            _camp._soldierCard[_camp.SoldierInPlace - 1].AgeText.SetText(age.ToString());
+            _camp._soldierCard[_camp.SoldierInPlace - 1].SituationText.SetText(situation);
             
             Debug.Log("Génération de soldat\nPrénom : " + firstName + "\nNom : " + lastName + "\nAge : " + age + "\nSituation : " + situation);
         }
