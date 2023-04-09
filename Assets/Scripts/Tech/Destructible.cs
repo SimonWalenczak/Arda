b3295284a4754003a76f1e5435be78cfe87d9b93
CT108 <jer59billemont@gmail.com>
2023-04-10 00:04:57 +0200

    Fix arbres explosion

--- Assets/Scripts/Tech/Destructible.cs
+++ Assets/Scripts/Tech/Destructible.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class Destructible : MonoBehaviour
 {
-    [SerializeField] GameObject DestroyedVersion; 
+    [SerializeField] GameObject DestroyedVersion;
+
+    BoxCollider boxCollider;
+
+    private void Awake()
+    {
+        boxCollider = gameObject.GetComponent<BoxCollider>();
+    }
 
 
     public void Destruction()
     {
+        boxCollider.enabled = false;
         Instantiate(DestroyedVersion, transform.position, transform.rotation);
         Destroy(gameObject);
     }
