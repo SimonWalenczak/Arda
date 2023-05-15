18e8eae8930c3eaa7e4ad6b651eb2a4493819a35
Frostork <simon.walenczak@lacatholille.fr>
2023-05-15 14:45:23 +0200

    update dialogue

--- Assets/Scripts/Night/GeneralDialog.cs
+++ Assets/Scripts/Night/GeneralDialog.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine.InputSystem;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class GeneralDialog : MonoBehaviour
 {
@@ -11,22 +12,99 @@ public class GeneralDialog : MonoBehaviour
     public bool CanUpgrade;
 
     [SerializeField] private TextMeshProUGUI _generalTextVisual;
+    [SerializeField] private List<Dialog> _generalTextFirstNight;
     [SerializeField] private List<Dialog> _generalText;
 
     [SerializeField] private int index = 0;
     [SerializeField] private GameObject BGLettre;
-    
+
+    private void Start()
+    {
+        GameData.NumberDays = 1;
+
+        CanTalk = false;
+        StartCoroutine(WaitingForTalk());
+    }
+
     private void Update()
     {
         if (CanTalk)
         {
             _generalTextVisual.gameObject.SetActive(true);
 
-            if (Gamepad.current.buttonSouth.wasPressedThisFrame)
+            if (GameData.NumberDays == 1)
             {
-                if (index < _generalText.Count - 1)
+                if (Gamepad.current.buttonSouth.wasPressedThisFrame)
                 {
-                    if (index >= 5)
+                    if (index < _generalTextFirstNight.Count - 1)
+                    {
+                        if (index >= 5)
+                        {
+                            if (_generalTextFirstNight[index].HavePaper == false)
+                            {
+                                index++;
+                            }
+                            else
+                            {
+                                if (_generalTextFirstNight[index].PaperActif == false)
+                                {
+                                    _generalTextFirstNight[index].PaperActif = true;
+                                    BGLettre.SetActive(true);
+                                    LetterAppearing(_generalTextFirstNight[index].Paper);
+                                }
+                                else
+                                {
+                                    BGLettre.SetActive(false);
+                                    LetterDisappearing(_generalTextFirstNight[index].Paper);
+                                    index++;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            if (index == 0)
+                            {
+                                if (GameData.HadSaveSoldier)
+                                    index = 1;
+                                else
+                                    index = 3;
+                            }
+                            else
+                            {
+                                if (_generalTextFirstNight[index].PaperActif == false)
+                                {
+                                    _generalTextFirstNight[index].PaperActif = true;
+                                    BGLettre.SetActive(true);
+                                    LetterAppearing(_generalTextFirstNight[index].Paper);
+                                }
+                                else if (_generalTextFirstNight[index].PaperActif == true && (index == 2 || index == 4))
+                                {
+                                    BGLettre.SetActive(false);
+                                    LetterDisappearing(_generalTextFirstNight[index].Paper);
+                                    index = 5;
+                                }
+                                else if (_generalTextFirstNight[index].PaperActif == true)
+                                {
+                                    BGLettre.SetActive(false);
+                                    LetterDisappearing(_generalTextFirstNight[index].Paper);
+                                    index++;
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        CanUpgrade = true;
+                    }
+                }
+
+                _generalTextVisual.SetText(_generalTextFirstNight[index].DialogText);
+            }
+            else
+            {
+                if (Gamepad.current.buttonSouth.wasPressedThisFrame)
+                {
+                    if (index < _generalText.Count - 1)
                     {
                         if (_generalText[index].HavePaper == false)
                         {
@@ -50,43 +128,12 @@ public class GeneralDialog : MonoBehaviour
                     }
                     else
                     {
-                        if (index == 0)
-                        {
-                            if (GameData.HadSaveSoldier)
-                                index = 1;
-                            else
-                                index = 3;
-                        }
-                        else
-                        {
-                            if (_generalText[index].PaperActif == false)
-                            {
-                                _generalText[index].PaperActif = true;
-                                BGLettre.SetActive(true);
-                                LetterAppearing(_generalText[index].Paper);
-                            }
-                            else if (_generalText[index].PaperActif == true && (index == 2 || index == 4))
-                            {
-                                BGLettre.SetActive(false);
-                                LetterDisappearing(_generalText[index].Paper);
-                                index = 5;
-                            }
-                            else if (_generalText[index].PaperActif == true)
-                            {
-                                BGLettre.SetActive(false);
-                                LetterDisappearing(_generalText[index].Paper);
-                                index++;
-                            }
-                        }
+                        CanUpgrade = true;
                     }
                 }
-                else
-                {
-                    CanUpgrade = true;
-                }
-            }
 
-            _generalTextVisual.SetText(_generalText[index].DialogText);
+                _generalTextVisual.SetText(_generalText[index].DialogText);
+            }
         }
     }
 
@@ -98,5 +145,11 @@ public class GeneralDialog : MonoBehaviour
     private void LetterDisappearing(GameObject letter)
     {
         StartCoroutine(letter.GetComponent<Letter>().Disappearing());
+    }
+
+    IEnumerator WaitingForTalk()
+    {
+        yield return new WaitForSeconds(3);
+        CanTalk = true;
     }
 }
