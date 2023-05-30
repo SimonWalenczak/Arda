47b3388f4099d70349e9d4333fd62bce04d2fdf5
CT-108 <jer59billemont@gmail.com>
2023-05-30 03:01:25 +0200

    CAM FONCTIONELLE AAAAAAA JSUIS TOUT NU

--- Assets/Scripts/Tech/FollowCamera.cs
+++ Assets/Scripts/Tech/FollowCamera.cs
@@ -12,18 +12,13 @@ public class FollowCamera : MonoBehaviour
     public float distance = 2f;
 
     private Vector3 offsetX;
-    private Vector3 startPos;
-    //private Vector3 offsetY;
-
-    
-    public float smoothSpeed = 0.125f; // Speed of camera movement
+    private Vector3 offsetY;
 
 
     void Start()
     {
-        startPos = transform.position;
-        offsetX = new Vector3(0, height, -distance);
-        //offsetY = new Vector3(0, 0, distance);
+        offsetX = new Vector3(0, 0, -distance);
+        offsetY = new Vector3(0, height, 0);
     }
 
     void LateUpdate()
@@ -42,10 +37,34 @@ public class FollowCamera : MonoBehaviour
         //    transform.LookAt(player.position);
         //}
 
+        // Keep the camera above the terrain
+        int layerMask = 1 << 6;
+
+        RaycastHit hit;
+        //Debug.DrawRay(player.position, -transform.up, Color.yellow, 1);
+
+        if (Physics.Raycast(transform.position, -transform.up, out hit, layerMask))
+        {
+            float terrainHeight = hit.point.y; // Adjust this value based on your terrain height
+            offsetY = new Vector3(0, height + terrainHeight, 0);
+
+            //if (transform.position.y < terrainHeight)
+            //{
+            //    transform.position = new Vector3(transform.position.x, terrainHeight, transform.position.z);
+            //}
+        }
+
         offsetX = Quaternion.AngleAxis(joystickInput.x * turnSpeed, Vector3.up) * offsetX;
-        //offsetY = Quaternion.AngleAxis(joystickInput.y * turnSpeed, Vector3.right) * offsetY;
-        transform.position = player.position + offsetX;
+
+        Vector3 newPos = new Vector3(player.position.x + offsetX.x, offsetY.y, player.position.z + offsetX.z);
+
+
+        transform.position = newPos;
         transform.LookAt(player.position);
+
+
+
+
     }
 
     public void GetXvalue(InputAction.CallbackContext ctx)
