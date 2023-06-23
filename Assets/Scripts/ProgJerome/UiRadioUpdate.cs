ff6b57a6164ce1a1d60bb60a9757dac5ea4c162d
SimonWalenczak <92367148+SimonWalenczak@users.noreply.github.com>
2023-06-23 17:09:23 +0200

    fix bug scene final

--- Assets/Scripts/ProgJerome/UiRadioUpdate.cs
+++ Assets/Scripts/ProgJerome/UiRadioUpdate.cs
@@ -15,48 +15,11 @@ public class UiRadioUpdate : MonoBehaviour
 
     public static UiRadioUpdate Instance;
 
-    public int indexSoldier;
-
     public List<RectTransform> SoldiersPanelOrigin;
-    public RectTransform lastSelected;
-    public RectTransform actualSelected;
-    
+
     private void Awake()
     {
         Instance = this;
-        indexSoldier = 0;
-        lastSelected = SoldiersPanelOrigin[0];
-        actualSelected = SoldiersPanelOrigin[0];
-        actualSelected.localPosition = Vector3.Lerp(actualSelected.localPosition, new Vector3(-280, actualSelected.localPosition.y, actualSelected.localPosition.z), 1);
-    }
-    
-    private void Update()
-    {
-        if (Gamepad.current.dpad.down.wasPressedThisFrame /*|| Gamepad.current.dpad.right.wasPressedThisFrame*/)
-        {
-            indexSoldier++;
-            if (indexSoldier > DataCenterDay.Instance.CurrentSoldiers.Count - 1)
-                indexSoldier = 0;
-            
-            lastSelected = actualSelected;
-            actualSelected = SoldiersPanelOrigin[indexSoldier];
-            
-            lastSelected.localPosition = Vector3.Lerp(lastSelected.localPosition, new Vector3(-500, lastSelected.localPosition.y, lastSelected.localPosition.z), 500 * Time.deltaTime);
-            actualSelected.localPosition = Vector3.Lerp(actualSelected.localPosition, new Vector3(-280, actualSelected.localPosition.y, actualSelected.localPosition.z), 1);
-        }
-        
-        if (Gamepad.current.dpad.up.wasPressedThisFrame /*||Gamepad.current.dpad.left.wasPressedThisFrame*/)
-        {
-            indexSoldier--;
-            if (indexSoldier < 0)
-                indexSoldier = DataCenterDay.Instance.CurrentSoldiers.Count - 1;
-        
-            lastSelected = actualSelected;
-            actualSelected = SoldiersPanelOrigin[indexSoldier];
-            
-            lastSelected.localPosition = Vector3.Lerp(lastSelected.localPosition, new Vector3(-365, lastSelected.localPosition.y, lastSelected.localPosition.z), 1);
-            actualSelected.localPosition = Vector3.Lerp(actualSelected.localPosition, new Vector3(-280, actualSelected.localPosition.y, actualSelected.localPosition.z), 1);
-        }
     }
 
     public void ApplyInfoSoldier(int i)
