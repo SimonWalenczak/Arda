aed7c606e3dd57e8e045b69b400295c7547116df
P3D\jbillemont <jer59billemont@gmail.com>
2023-06-26 18:46:58 +0200

    FX roue (reste à synchro la vitesse et vamos)

--- Assets/Scripts/Tech/WheelFX.cs
+++ Assets/Scripts/Tech/WheelFX.cs
@@ -4,15 +4,62 @@ using UnityEngine;
 
 public class WheelFX : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    ArcadeCar arcadeCar;
+
+    public List<ParticleSystem> mudLeft = new List<ParticleSystem>();
+    public List<ParticleSystem> mudRight = new List<ParticleSystem>(); 
+
+    float maxSpeedValue;
+
+    private void Awake()
     {
-        
+        arcadeCar = GetComponent<ArcadeCar>();
+        maxSpeedValue = GetMaxValueFromCurve(arcadeCar.accelerationCurve);
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        for (int i = 0; i < arcadeCar.axles.Length; i++)
+        {
+            if (arcadeCar.axles[i].wheelDataL.isOnGround)
+            {
+                mudLeft[i].Play();
+            }
+            else
+            {
+                mudLeft[i].Stop();
+            }
+
+            if (arcadeCar.axles[i].wheelDataR.isOnGround)
+            {
+                mudRight[i].Play();
+            }
+            else
+            {
+                mudRight[i].Stop();
+            }
+        }
     }
+
+    float GetMaxValueFromCurve(AnimationCurve curve)
+    {
+        float maxValue = float.MinValue;
+
+        // Iterate through all the keys in the curve
+        for (int i = 0; i < curve.length; i++)
+        {
+            Keyframe keyframe = curve[i];
+
+            // Compare the value of the current keyframe with the current maximum value
+            if (keyframe.value > maxValue)
+            {
+                maxValue = keyframe.value;
+            }
+        }
+
+        return maxValue;
+    }
+
+
 }
